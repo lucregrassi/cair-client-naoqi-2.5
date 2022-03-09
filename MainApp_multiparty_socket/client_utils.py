1fa45476917dc66f834801ee5af5d3648f879f57
Lucrezia <lucre.grassi@gmail.com>
2022-03-09 16:50:20 +0100

    updated version of client for multiparty

--- MainApp_multiparty_socket/client_utils.py
+++ MainApp_multiparty_socket/client_utils.py
@@ -6,10 +6,10 @@ import requests
 import time
 import numpy as np
 
-registration_host = "130.251.13.147"
+registration_host = "130.251.13.172"
 registration_port = 9091
 
-server_IP = "130.251.13.147"
+server_IP = "130.251.13.172"
 BASE = "http://" + server_IP + ":5000/CAIR_hub"
 
 
@@ -21,8 +21,8 @@ class Utils(object):
         self.memory = ALProxy("ALMemory")
         self.animated_speech = ALProxy("ALAnimatedSpeech")
         self.configuration = {"bodyLanguageMode": "contextual"}
-        self.speakers_info_file_path = "/data/home/nao/.local/share/PackageManager/apps/cairapp/speakers_info.json"
-        self.speakers_stats_file_path = "/data/home/nao/.local/share/PackageManager/apps/cairapp/speakers_stats.json"
+        self.speakers_info_file_path = "/data/home/nao/.local/share/PackageManager/apps/mainapp_multiparty/speakers_info.json"
+        self.speakers_stats_file_path = "/data/home/nao/.local/share/PackageManager/apps/mainapp_multiparty/speakers_stats.json"
         
         try:
             # self.voice_speed = "\\RSPD=100\\"
@@ -43,6 +43,7 @@ class Utils(object):
     def acquire_initial_state(self, profile_id, user_name):
         # Try to contact the server
         data = {"profile_id": profile_id}
+        self.logger("Acquiring user initial state")
         resp = requests.put(BASE, data=json.dumps(data), verify=False)
         dialogue_state = resp.json()['dialogue_state']
 
@@ -58,10 +59,10 @@ class Utils(object):
         # If it's not the first registration of a user
         if profile_id != "00000000-0000-0000-0000-000000000000":
             # Load the information about the already existing users
-            with open(self.speakers_info_file_path, 'rb') as info:
+            with open(self.speakers_info_file_path, 'r') as info:
                 profiles_dict = json.load(info)
             # Load the statistics about the interactions with existing users
-            with open(self.speakers_stats_file_path, 'rb') as stats:
+            with open(self.speakers_stats_file_path, 'r') as stats:
                 speakers_stats = json.load(stats)
             # The dimension of the square matrix will coincide with the length of the array containing the profile ids
             matrix_size = len(speakers_stats["mapping_index_speaker"])
@@ -99,8 +100,9 @@ class Utils(object):
                               "successive_interaction_prob": [[0.0]],
                               "average_topic_distance": [[0.0]],
                               "mapping_index_speaker": [profile_id],
-                              "a_priori_prob": [0.0]}
-
+                              "a_priori_prob": [0.0], 
+                              "speakers_moving_window": []}
+                              
         self.animated_speech.say(self.voice_speed + "Updating statistics and info", self.configuration)
         # Update the stats in the file
         with open(self.speakers_stats_file_path, 'w') as stats:
