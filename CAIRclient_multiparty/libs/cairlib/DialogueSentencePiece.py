4a6cf4848d44b02d1803d52557511f73e14fbd8b
lucregrassi <lucre.grassi@gmail.com>
2024-07-01 17:43:41 +0200

    updated client

--- CAIRclient_multiparty/libs/cairlib/DialogueSentencePiece.py
+++
@@ -1,5 +0,0 @@
-class DialogueSentencePiece:
-    def __init__(self, sentence_type=None, sentence=None, profile_id=None):
-        self.sentence_type = sentence_type
-        self.sentence = sentence
-        self.profile_id = profile_id
